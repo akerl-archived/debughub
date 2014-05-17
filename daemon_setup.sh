749eb1c19ed2616e9d8c554dc4352351d52d2409
Les Aker <me@lesaker.org>
2014-05-17 12:47:07 -0400

    fix daemon install

--- daemon_setup.sh
+++ daemon_setup.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname "${BASH_SOURCE[0]}")
 
-pacman -Syu --needed nginx
+pacman -Syu --needed --noconfirm nginx
 
 unchanged_sha="8afcd712bc47ee098a3d06f7e388a3d9d28f07de  /etc/nginx/nginx.conf"
 if [ "$(shasum /etc/nginx/nginx.conf)" != "$unchanged_sha" ] ; then
@@ -12,5 +12,5 @@ fi
 
 cp nginx.conf /etc/nginx/nginx.conf
 sed -i "s|FAKEHUB_ROOT|$(pwd)/src|" /etc/nginx/nginx.conf
-systemctl nginx restart
+systemctl restart nginx
 
