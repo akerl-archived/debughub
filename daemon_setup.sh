b7f8fcc838daf71c73046853065d7434e4f47319
Les Aker <me@lesaker.org>
2014-05-17 11:36:56 -0400

    initial commit, installs nginx

---
+++ daemon_setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd $(dirname "${BASH_SOURCE[0]}")
+
+pacman -Syu --needed nginx
+
+unchanged_sha="8afcd712bc47ee098a3d06f7e388a3d9d28f07de  /etc/nginx/nginx.conf"
+if [ "$(shasum /etc/nginx/nginx.conf)" != "$unchanged_sha" ] ; then
+    echo "Nginx config has been modified; bailing"
+    exit 1
+fi
+
+cp nginx.conf /etc/nginx/nginx.conf
+sed -i "s|FAKEHUB_ROOT|$(pwd)/src|" /etc/nginx/nginx.conf
+systemctl nginx restart
+
